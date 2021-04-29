cd6188e402c4ce62795c6d390655113530902b1f
Miracle847 <2678988649@qq.com>
2021-04-29 11:12:43 +0800

    '工程初始化'

---
+++ routes/register.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    console.log(req.query)
+    if(req.query.newusername.length()>0){
+        res.send('这是注册界面');
+    }
+});
+
+module.exports = router;
